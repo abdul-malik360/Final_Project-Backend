966c493e595af50a17e194a1daafff0aa890b7bc
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-08-26 16:11:49 +0200

    added admin route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Import modules needed
 import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 
 # putting all the tables in a class
@@ -59,3 +59,82 @@ QatTables()     # calling the class
 # starting the Flask app
 app = Flask(__name__)   # allows you to use api
 app.debug = True    # when finds a bug, it continues to run
+
+
+@app.route('/admin', methods=["POST", "GET"])   # created a route that has both get and post methods
+def admin():    # function for all admin uses
+    response = {}   # making response an empty dictionary
+
+    if request.method == "POST":    # if the function is post the following code happen
+        fullname = request.form['Fullname']
+        username = request.form['Username']
+        password = request.form['Password']
+
+        with sqlite3.connect("QAT_Motors.db") as connect:
+            cursor = connect.cursor()
+            cursor.execute("INSERT INTO Admin("     # code that adds data to the table
+                           "Fullname,"
+                           "Username,"
+                           "Password) VALUES(?, ?, ?)", (fullname, username, password))
+            connect.commit()
+            response["message"] = "success"
+            response["status_code"] = 201
+        return response
+
+    if request.method == "GET":    # if the function is post the following code happen
+        response = {}
+
+        with sqlite3.connect("QAT_Motors.db") as connect:
+            cursor = connect.cursor()
+            cursor.execute("SELECT * FROM Admin")
+
+            admin_users = cursor.fetchall()
+
+        response['status_code'] = 200
+        response['data'] = admin_users
+        return response
+
+
+@app.route('/admin/<username>', methods=["PUT", "GET"])
+def edit_admin(username):
+    response = {}
+
+    if request.method == "PUT":
+        with sqlite3.connect("QAT_Motors.db") as connect:
+            incoming_data = dict(request.json)
+            new_data = {}
+
+            if incoming_data.get("Fullname") is not None:
+                new_data["Fullname"] = incoming_data.get("Fullname")
+                with sqlite3.connect("QAT_Motors.db") as connect:
+                    cursor = connect.cursor()
+                    cursor.execute("UPDATE Admin SET Fullname =? WHERE Username=?", (new_data["Fullname"], username))
+                    connect.commit()
+                    response['Fullname'] = "Fullname Update was successful"
+                    response['status_code'] = 200
+
+            if incoming_data.get("Username") is not None:
+                new_data['Username'] = incoming_data.get('Username')
+
+                with sqlite3.connect("QAT_Motors.db") as connect:
+                    cursor = connect.cursor()
+                    cursor.execute("UPDATE Admin SET Username =? WHERE Username=?", (new_data["Username"], username))
+                    connect.commit()
+                    response['Username'] = "Username Update was successful"
+                    response['status_code'] = 200
+
+            if incoming_data.get("Password") is not None:
+                new_data['Password'] = incoming_data.get('Password')
+
+                with sqlite3.connect("QAT_Motors.db") as connect:
+                    cursor = connect.cursor()
+                    cursor.execute("UPDATE Admin SET Password =? WHERE Username=?", (new_data["Password"], username))
+                    connect.commit()
+                    response['Password'] = "Password Update was successful"
+                    response['status_code'] = 200
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
