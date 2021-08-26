d9e33c656bc112cec17f27729947a9b40960f2b5
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-08-26 16:24:59 +0200

    added reg users and edit admin function

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # Import modules needed
 import sqlite3
+import re
+import phonenumbers
+
 from flask import Flask, request
+from flask_mail import Mail, Message
+from phonenumbers import NumberParseException
 
 
 # putting all the tables in a class
@@ -9,9 +14,10 @@ class QatTables:
         self.connect = sqlite3.connect('QAT_Motors.db')    # connecting sqlite3 to the database called QAT_Motors
         self.cursor = self.connect.cursor()
 
-        # creating a table for admin
-        self.connect.execute("CREATE TABLE IF NOT EXISTS Admin(Fullname TEXT NOT NULL,"   # command to create the table
-                             "Username TEXT NOT NULL PRIMARY KEY,"
+        # creating a table for admin    # command to create the table
+        self.connect.execute("CREATE TABLE IF NOT EXISTS Admin(Admin_ID INTEGER PRIMARY KEY AUTOINCREMENT,"  
+                             "Fullname TEXT NOT NULL,"   
+                             "Username TEXT NOT NULL UNIQUE,"
                              "Password TEXT NOT NULL)")
         print("Admin table created successfully")    # checking if table was created
 
@@ -50,7 +56,12 @@ class QatTables:
                              "CONSTRAINT fk_Vehicles FOREIGN KEY (Reg_Numb) REFERENCES Vehicles (Reg_Numb)"
                              "CONSTRAINT fk_Services FOREIGN KEY (Type) REFERENCES Services (Type))")
         print("Appointments table created successfully")
-
+        # x = phonenumbers.parse("+27764971338", "ZA")
+        # z = phonenumbers.is_possible_number(x)
+        # y = phonenumbers.is_valid_number(x)
+        # print(x)
+        # print(z)
+        # print(y)
         self.connect.close()    # closes connection to database
 
 
@@ -60,12 +71,20 @@ QatTables()     # calling the class
 app = Flask(__name__)   # allows you to use api
 app.debug = True    # when finds a bug, it continues to run
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'    # following code is used to send email's through flask
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = '62545a@gmail.com'
+app.config['MAIL_PASSWORD'] = 'zbuurwxtrtnotwvn'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)    # end of email code config
+
 
 @app.route('/admin', methods=["POST", "GET"])   # created a route that has both get and post methods
-def admin():    # function for all admin uses
+def admin():    # function to add and show admin users
     response = {}   # making response an empty dictionary
 
-    if request.method == "POST":    # if the function is post the following code happen
+    if request.method == "POST":    # if the function method is post admin registers data
         fullname = request.form['Fullname']
         username = request.form['Username']
         password = request.form['Password']
@@ -81,7 +100,7 @@ def admin():    # function for all admin uses
             response["status_code"] = 201
         return response
 
-    if request.method == "GET":    # if the function is post the following code happen
+    if request.method == "GET":    # if the function method is get, all data from the admin table is displayed
         response = {}
 
         with sqlite3.connect("QAT_Motors.db") as connect:
@@ -95,45 +114,90 @@ def admin():    # function for all admin uses
         return response
 
 
-@app.route('/admin/<username>', methods=["PUT", "GET"])
-def edit_admin(username):
+@app.route('/admin/<int:admin_id>', methods=["PUT", "GET"])   # created a route that has both put and get methods
+def edit_admin(admin_id):
     response = {}
 
-    if request.method == "PUT":
+    if request.method == "PUT":     # this method will edit the admin user's data
+        try:
+            fullname = request.json['Fullname']
+            username = request.json['Username']
+            password = request.json['Password']
+
+            with sqlite3.connect("QAT_Motors.db") as connect:
+                cursor = connect.cursor()
+                cursor.execute("UPDATE Admin SET Fullname =?, Username=?, Password=? WHERE Admin_ID=?",
+                               (fullname, username, password, admin_id))
+                connect.commit()
+                response['message'] = "Admin update was successful"
+                response['status_code'] = 200
+
+            return response
+
+        except ValueError:
+            response["message"] = "Admin update was unsuccessful, try again"
+            response["status_code"] = 209
+            return response
+
+    if request.method == "GET":     # this method will delete the admin user
         with sqlite3.connect("QAT_Motors.db") as connect:
-            incoming_data = dict(request.json)
-            new_data = {}
+            cursor = connect.cursor()
+            cursor.execute("DELETE FROM Admin WHERE Admin_ID=" + str(admin_id))
+            connect.commit()
+            response['status_code'] = 204
+            response['message'] = "Admin User deleted successfully"
+        return response
 
-            if incoming_data.get("Fullname") is not None:
-                new_data["Fullname"] = incoming_data.get("Fullname")
+
+@app.route('/client', methods=["POST", "GET"])      # a route for clients with post and get methods
+def client():   # a function to add and view users
+    response = {}
+
+    if request.method == "POST":    # this method adds clients
+        try:
+            name = request.form['Name']
+            surname = request.form['Surname']
+            email = request.form['Email']
+            cell = request.form['Cell']
+            address = request.form['Address']
+            username = request.form['Username']
+            password = request.form['Password']
+
+            regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'    # code to validate email entered
+
+            x = phonenumbers.parse(cell, "ZA")    # code to validate cell number
+            y = phonenumbers.is_possible_number(x)
+            z = phonenumbers.is_valid_number(x)
+
+            # entry will only be accepted if email address and cell number is valid
+            if re.search(regex, email, z):
                 with sqlite3.connect("QAT_Motors.db") as connect:
                     cursor = connect.cursor()
-                    cursor.execute("UPDATE Admin SET Fullname =? WHERE Username=?", (new_data["Fullname"], username))
+                    cursor.execute("INSERT INTO Clients("
+                                   "Name,"
+                                   "Surname,"
+                                   "Email,"
+                                   "Cell,"
+                                   "Address,"
+                                   "Username,"
+                                   "Password) VALUES(?, ?, ?, ?, ?, ?, ?)",
+                                   (name, surname, email, cell, address, username, password))
                     connect.commit()
-                    response['Fullname'] = "Fullname Update was successful"
-                    response['status_code'] = 200
 
-            if incoming_data.get("Username") is not None:
-                new_data['Username'] = incoming_data.get('Username')
+                msg = Message('Welcome To QAT Motors', sender='62545a@gmail.com', recipients=[email])
+                msg.body = "Thank you for registering to our services Mr " + surname
+                mail.send(msg)
 
-                with sqlite3.connect("QAT_Motors.db") as connect:
-                    cursor = connect.cursor()
-                    cursor.execute("UPDATE Admin SET Username =? WHERE Username=?", (new_data["Username"], username))
-                    connect.commit()
-                    response['Username'] = "Username Update was successful"
-                    response['status_code'] = 200
+                response["message"] = "Success, Check Email"
+                response["status_code"] = 201
 
-            if incoming_data.get("Password") is not None:
-                new_data['Password'] = incoming_data.get('Password')
+            else:
+                response['message'] = "Invalid Email Address"
 
-                with sqlite3.connect("QAT_Motors.db") as connect:
-                    cursor = connect.cursor()
-                    cursor.execute("UPDATE Admin SET Password =? WHERE Username=?", (new_data["Password"], username))
-                    connect.commit()
-                    response['Password'] = "Password Update was successful"
-                    response['status_code'] = 200
+        except NumberParseException:
+            response['message'] = "Invalid Cell Number"
 
-    return response
+        return response
 
 
 if __name__ == '__main__':
